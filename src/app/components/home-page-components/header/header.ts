ffb40dde9d9bd79953dfd933731ba9fb934d70e3
Nilo18 <nikalongurashvili@gmail.com>
2026-09-01 15:00:02 +0400

    features: finish the header and hero components on the home page

--- src/app/components/home-page-components/header/header.ts
+++ src/app/components/home-page-components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss',
 })
 export class Header {
+  isScrolled: WritableSignal<boolean> = signal(false)
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop
+
+    this.isScrolled.set(scrollPosition > 50)
+  }
 }
