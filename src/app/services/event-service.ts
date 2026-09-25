701a218dc8ebb5a782cd51f2da10a5072b543ffa
Nilo18 <nikalongurashvili@gmail.com>
2026-09-25 18:22:46 +0400

    feature: start making the events section on staff page interactive

--- src/app/services/event-service.ts
+++ src/app/services/event-service.ts
@@ -1,6 +1,6 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { BackendUrlHolderService } from './backend-url-holder-service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ClientType } from '../components/request-event-components/event-request-form/event-request.models';
 import { firstValueFrom } from 'rxjs';
 import { GenericResponse, OtpResponse } from './home-service';
@@ -12,7 +12,7 @@ export interface ClientDTO {
   phone: string;
 }
 
-export interface EventDTO {
+export interface EventCreationRequest {
   eventType: string;
   date: string;         // ISO-8601 Date string format: "YYYY-MM-DD"
   totalCost?: number;   // BigDecimal maps to number (optional since it can be null)
@@ -23,10 +23,79 @@ export interface EventDTO {
 
 export interface EventOrderRequest {
   client: ClientDTO;
-  event: EventDTO;
+  event: EventCreationRequest;
   menuItemIds: number[]; // Java List<Long> maps directly to number[]
   transactionKey: string;
   otp: string;
+}
+
+export interface PageResponse<T> {
+  content: T[];
+  pageNumber: number;
+  pageSize: number;
+  totalElements: number;
+}
+
+export enum EventStatus {
+  REQUESTED = "REQUESTED",
+  PLANNING = "PLANNING",
+  CONFIRMED = "CONFIRMED",
+  COMPLETED = "COMPLETED",
+  REJECTED = "REJECTED",
+  CANCELLED = "CANCELLED"
+}
+
+export enum EventType {
+  WEDDING = "WEDDING",
+  CORPORATE_EVENT = "CORPORATE_EVENT",
+  CONFERENCE = "CONFERENCE",
+  OFFICIAL_RECEPTION = "OFFICIAL_RECEPTION",
+  ANNIVERSARY = "ANNIVERSARY",
+  BIRTHDAY = "BIRTHDAY",
+  GALA_DINNER = "GALA_DINNER",
+  PRODUCT_LAUNCH = "PRODUCT_LAUNCH",
+  PRIVATE_PARTY = "PRIVATE_PARTY",
+  OTHER = "OTHER"
+}
+
+export enum EventLocation {
+  AURUM_BANQUET_HALL = "AURUM_BANQUET_HALL",
+  AURUM_CONFERENCE_HALL = "AURUM_CONFERENCE_HALL",
+  PRIVATE_RESIDENCE = "PRIVATE_RESIDENCE",
+  PARTNER_VENUE = "PARTNER_VENUE",
+  HOTEL = "HOTEL",
+  RESTAURANT = "RESTAURANT",
+  OUTDOOR_VENUE = "OUTDOOR_VENUE",
+  HISTORICAL_VENUE = "HISTORICAL_VENUE",
+  CORPORATE_OFFICE = "CORPORATE_OFFICE",
+  OTHER = "OTHER"
+}
+
+export interface EventDTO {
+  clientName: string;
+  eventType: EventType;
+  date: string;
+  totalCost: number;
+  guestCount: number;
+  location: EventLocation;
+  status: EventStatus;
+}
+
+export interface EventQuery {
+  page?: number;
+  size?: number;
+  eventType?: EventType;
+  costFrom?: number;
+  costTo?: number;
+  guestFrom?: number;
+  guestTo?: number;
+  eventFrom?: string; // ISO თარიღის ფორმატი "YYYY-MM-DD"
+  eventTo?: string;   // ISO თარიღის ფორმატი "YYYY-MM-DD"
+  location?: EventLocation;
+  status?: EventStatus;
+  sortBy?: string;
+  sortDirection?: string;
+  search?: string;
 }
 
 @Injectable({
@@ -36,6 +105,15 @@ export class EventService {
   private backendUrlHolder = inject(BackendUrlHolderService)
   private baseUrl = this.backendUrlHolder.getBaseUrl()
   private http = inject(HttpClient)
+  private eventQuery = signal<EventQuery>(
+    {
+      page: 0,
+      size: 10,
+      sortBy: "",
+      sortDirection: "",
+      search: ""
+    }
+  )
 
   async verifyCreateEventRequest(email: string) {
     try {
@@ -57,5 +135,32 @@ export class EventService {
       console.log("Couldn't create event: ", error)
       throw error;
     }
+  }
+
+  async getEvents(query: EventQuery) {
+    try {
+      const cleanedQuery = this.removeEmptyProperties(query);
+      const httpParams = new HttpParams({ fromObject: cleanedQuery });
+      const res = await firstValueFrom(this.http.get<PageResponse<EventDTO>>(`${this.baseUrl}/api/event/verify`, 
+        {
+          params: httpParams
+        }
+      ))
+      console.log(res)
+      return res;
+    } catch (error) {
+      console.log("Couldn't verify event creation: ", error)
+      throw error;
+    }
+  }
+
+  private removeEmptyProperties(obj: any): any {
+    const result: any = {};
+    Object.keys(obj).forEach((key) => {
+      if (obj[key] !== null && obj[key] !== undefined && obj[key] !== '') {
+        result[key] = String(obj[key]);
+      }
+    });
+    return result;
   }
 }
